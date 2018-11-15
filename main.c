e0cf018ec773abb88c7f69735ea2f097d7e97132
choihj0202 <helenchoi22@naver.com>
2018-11-15 16:06:48 +0900

    exercise3

--- main.c
+++ main.c
@@ -3,17 +3,43 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-char *proverb = "All that glister is not gold.";
-void setPointer(char **q)
-{
-	*q = proverb;
+int sum(int a, int b)
+{	
+	return a+b;
+} 
+
+int subtract(int a, int b)
+{	
+	return a-b;
+}
+
+int time(int a, int b)
+{	
+	return a*b;
+}
+
+int divide(int a, int b)
+{	
+	return a/b;
 }
 
 int main(int argc, char *argv[]) {
 	
-	char *p = "zzz";
-	setPointer(&p); 
-	printf("%s\n", p);
+	int num1, num2;
+	int (*calculation)(int, int);
+	char ch;
 	
+	printf("input calculation : ");
+	scanf("%d %c %d", &num1, &ch, &num2);
+	
+	switch (ch)
+	{
+		case '+' : calculation = sum; break;
+		case '-' : calculation = subtract; break;
+		case '*' : calculation = time; break;
+		case '/' : calculation = divide; break;
+	}
+	
+	printf("output : %d\n", calculation(num1, num2));
 	return 0;
 }
